02e754f2dff28a848b3ca3c3644b4ec467aba0fb
GSNugroho <gnugroho11@gmail.com>
2022-05-24 15:26:13 +0700

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.listen(8000, function(){
-    console.log('Server is running on port 8000');
+const mockUserData = [
+    {name: 'Mark'},
+    {name: 'Jill'}
+];
+
+app.get('/user', function(req, res){
+   res.json({
+        success: true,
+        message: 'Got user data',
+        userData: mockUserData
+   });
+});
+
+app.listen(3000, function(){
+    console.log('Listening on port 3000');
 });
